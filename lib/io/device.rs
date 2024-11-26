511abe93ea705119b94ec7f8d330998fa8a87864
Fritiof Rusck <fritiof@rusck.se>
2024-11-26 10:30:12 +0100

    feat: implement Device

--- lib/io/device.rs
+++ lib/io/device.rs
@@ -1,6 +1,25 @@
 use crate::{io, Data};
+use std::{cell::RefCell, rc::Rc};
 
 pub trait Device<T>: io::Interruptable + Data<T> + std::fmt::Debug {
     // Clock the device
     fn clock(&mut self) {}
+}
+
+impl<K> io::Interruptable for Rc<RefCell<K>>
+where
+    K: io::Interruptable
+{
+    fn interrupt(&self) -> Option<u32> {
+        self.borrow_mut().interrupt()
+    }
+}
+
+impl<K, T> Device<T> for Rc<RefCell<K>>
+where
+    K: Device<T>
+{
+    fn clock(&mut self) {
+        self.borrow_mut().clock();
+    }
 }
